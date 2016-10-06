4817609e910051b2d6c947fb5d347beb1daf72d3
Vu Phan <vuphan314@gmail.com>
2016-10-05 21:14:56 -0500

    dir code/engine/ is for executables only

---
+++ code/engine/README.md
@@ -0,0 +1,3 @@
+# STANDALONE EXECUTABLE
+
+source: `../knengine/`
--- code/knengine/kn_engine.bat
+++ code/knengine/kn_engine.bat
@@ -25,19 +25,19 @@ goto starting
 
 :bundling
     set spec_man=engine_man.spec
-    set work_path=..\build\
-    set dist_path=..
+    set work_path=%engine_path%build\
+    set dist_path=%engine_path%
     set knengine_path=%CD%
 
     set pyi_makespec=pyi-makespec %engine_py% -F
     set pyi_bundle=pyinstaller %spec_man% --workpath=%work_path% --distpath=%dist_path%
 
     REM %pyi_makespec%
-    %pyi_bundle%
+    REM %pyi_bundle%
 
     cd %dist_path%
     %engine_exe%
-    %engine_exe% knengine\examples\demo.kn
+    %engine_exe% examples\demo.kn
     cd %knengine_path%
 
     goto ending
@@ -47,7 +47,9 @@ goto starting
     cls
     setlocal enabledelayedexpansion
 
+    set engine_path=..\engine\
     set engine_exe=engine.exe
+
     set engine_py=engine.py
     set kn_engine_py=kn_engine.py
 
