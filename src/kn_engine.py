d76f92c962d413319b02546daecd5be433155371
Vu Phan <vuphan314@gmail.com>
2016-11-14 09:56:47 -0600

    renaming modules to avoid name clash

--- src/kn_engine.py
+++ src/kn_engine.py
@@ -154,7 +154,7 @@ def main() -> None:
         write_output_files(kn_path, force, keep)
 
     time_taken = time.time() - time_start
-    print('Time taken: {} seconds.'.format(time_taken))
+    print('\n' 'Time taken: {} seconds.'.format(time_taken))
 
 if __name__ == '__main__':
     main()
